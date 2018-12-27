0018e95d37c7dceb7a597bc1434bafbf91fa595c
Reijhanniel Jearl Campos <devcsrj@apache.org>
2018-12-27 14:53:17 +0800

    :tada: Migrate to maven

---
+++ EasyGraph/src/main/java/de/amr/easy/graph/api/traversal/TraversalState.java
@@ -0,0 +1,10 @@
+package de.amr.easy.graph.api.traversal;
+
+/**
+ * States of a graph vertex with regards to graph traversal.
+ * 
+ * @author Armin Reichert
+ */
+public enum TraversalState {
+	UNVISITED, VISITED, COMPLETED;
+}
