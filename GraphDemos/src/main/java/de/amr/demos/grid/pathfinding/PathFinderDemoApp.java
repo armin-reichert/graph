9f96978a0b38835d30d52626beafffe531c5da95
Armin Reichert <armin.reichert@web.de>
2019-02-25 12:43:44 +0100

    improved

--- GraphDemos/src/main/java/de/amr/demos/grid/pathfinding/PathFinderDemoApp.java
+++ GraphDemos/src/main/java/de/amr/demos/grid/pathfinding/PathFinderDemoApp.java
@@ -41,7 +41,6 @@ import de.amr.graph.pathfinder.api.TraversalState;
 import de.amr.graph.pathfinder.impl.AStarSearch;
 import de.amr.graph.pathfinder.impl.BreadthFirstSearch;
 import de.amr.graph.pathfinder.impl.DijkstraSearch;
-import de.amr.graph.pathfinder.impl.GraphSearch;
 import de.amr.util.StopWatch;
 
 /**
@@ -71,9 +70,9 @@ public class PathFinderDemoApp {
 		BFS, Dijkstra, AStar;
 	}
 
-	private GridGraph2D<Tile, Integer> map;
-	private PathFinderAlgorithm algorithm;
-	private GraphSearch<Tile, Integer> pathFinder;
+	private GridGraph2D<Tile, Double> map;
+	private BreadthFirstSearch<Tile, Double> pathFinder;
+	private PathFinderAlgorithm selectedAlgorithm;
 	private int source;
 	private int target;
 	private BitSet solution;
@@ -120,8 +119,8 @@ public class PathFinderDemoApp {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			JComponent source = (JComponent) e.getSource();
-			algorithm = (PathFinderAlgorithm) source.getClientProperty("algorithm");
-			System.out.println("Selected " + algorithm);
+			selectedAlgorithm = (PathFinderAlgorithm) source.getClientProperty("algorithm");
+			System.out.println("Selected " + selectedAlgorithm);
 			newPathFinder();
 			updatePath();
 		}
@@ -166,8 +165,9 @@ public class PathFinderDemoApp {
 	};
 
 	public PathFinderDemoApp(int numCols, int numRows, int canvasSize) {
-		algorithm = PathFinderAlgorithm.AStar;
-		map = new GridGraph<>(numCols, numRows, Top8.get(), v -> Tile.FREE, (u, v) -> 1, UndirectedEdge::new);
+		selectedAlgorithm = PathFinderAlgorithm.AStar;
+		map = new GridGraph<>(numCols, numRows, Top8.get(), v -> Tile.FREE, (u, v) -> map.euclidean(u, v),
+				UndirectedEdge::new);
 		map.fill();
 		cellSize = canvasSize / numCols;
 		source = map.cell(GridPosition.TOP_LEFT);
@@ -213,7 +213,7 @@ public class PathFinderDemoApp {
 			JRadioButtonMenuItem rb = new JRadioButtonMenuItem(actionSelectAlgorithm);
 			rb.putClientProperty("algorithm", algorithm);
 			rb.setText(algorithm.name());
-			rb.setSelected(algorithm == this.algorithm);
+			rb.setSelected(algorithm == this.selectedAlgorithm);
 			bg.add(rb);
 			popupMenu.add(rb);
 		}
@@ -268,10 +268,10 @@ public class PathFinderDemoApp {
 			return "";
 		}
 		if (pathFinder instanceof AStarSearch) {
-			AStarSearch<Tile, Integer> astar = (AStarSearch<Tile, Integer>) pathFinder;
+			AStarSearch<Tile, Double> astar = (AStarSearch<Tile, Double>) pathFinder;
 			return String.format("%.0f", astar.getScore(cell));
 		} else if (pathFinder instanceof BreadthFirstSearch) {
-			BreadthFirstSearch<Tile, Integer> bfs = (BreadthFirstSearch<Tile, Integer>) pathFinder;
+			BreadthFirstSearch<Tile, Double> bfs = pathFinder;
 			return String.format("%.0f", bfs.getCost(cell));
 		}
 		return "";
@@ -294,7 +294,7 @@ public class PathFinderDemoApp {
 	}
 
 	private void newPathFinder() {
-		switch (algorithm) {
+		switch (selectedAlgorithm) {
 		case AStar:
 			pathFinder = new AStarSearch<>(map, i -> i, this::getDistance);
 			break;
@@ -312,10 +312,12 @@ public class PathFinderDemoApp {
 		watch.start();
 		List<Integer> path = pathFinder.findPath(source, target);
 		watch.stop();
-		System.out.println(String.format("Path finding (%s): %.4f seconds", algorithm, watch.getSeconds()));
+		System.out
+				.println(String.format("Path finding (%s): %.4f seconds", selectedAlgorithm, watch.getSeconds()));
 		solution = new BitSet(map.numVertices());
 		path.forEach(solution::set);
 		System.out.println(String.format("Path length: %d", path.size()));
+		System.out.println(String.format("Path cost: %.2f", pathFinder.getCost(target)));
 	}
 
 	private void updatePath() {
