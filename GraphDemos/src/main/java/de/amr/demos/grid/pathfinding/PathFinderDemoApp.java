f16e55a3015eefe5a41124f9c569f5ff20d00cbe
Armin Reichert <armin.reichert@web.de>
2019-02-26 09:15:17 +0100

    reworked

--- GraphDemos/src/main/java/de/amr/demos/grid/pathfinding/PathFinderDemoApp.java
+++ GraphDemos/src/main/java/de/amr/demos/grid/pathfinding/PathFinderDemoApp.java
@@ -4,24 +4,11 @@ import static de.amr.demos.grid.pathfinding.PathFinderDemoApp.Tile.FREE;
 import static de.amr.demos.grid.pathfinding.PathFinderDemoApp.Tile.WALL;
 import static java.lang.Math.min;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.Font;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseMotionListener;
 import java.util.BitSet;
 import java.util.List;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.BorderFactory;
-import javax.swing.JPopupMenu;
 import javax.swing.UIManager;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
@@ -31,10 +18,6 @@ import de.amr.graph.grid.api.GridPosition;
 import de.amr.graph.grid.api.Topology;
 import de.amr.graph.grid.impl.GridGraph;
 import de.amr.graph.grid.impl.Top8;
-import de.amr.graph.grid.ui.rendering.ConfigurableGridRenderer;
-import de.amr.graph.grid.ui.rendering.GridCanvas;
-import de.amr.graph.grid.ui.rendering.WallPassageGridRenderer;
-import de.amr.graph.pathfinder.api.TraversalState;
 import de.amr.graph.pathfinder.impl.AStarSearch;
 import de.amr.graph.pathfinder.impl.BreadthFirstSearch;
 import de.amr.graph.pathfinder.impl.DijkstraSearch;
@@ -72,98 +55,24 @@ public class PathFinderDemoApp {
 	private PathFinderAlgorithm algorithm;
 	private Topology topology;
 	private int passageWidthPct;
+	private int cellSize;
 	private int source;
 	private int target;
 	private final BitSet solution;
 
-	// UI
-	private int draggedCell;
-	private int popupCell;
-	private int cellSize;
 	private PathFinderUI window;
-	private GridCanvas canvas;
-	private JPopupMenu popupMenu;
-
-	private Action actionSetSource = new AbstractAction("Set Source Here") {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			setSource(popupCell);
-			popupCell = -1;
-			updatePath();
-		}
-	};
-
-	private Action actionSetTarget = new AbstractAction("Set Target Here") {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			setTarget(popupCell);
-			popupCell = -1;
-			updatePath();
-		}
-	};
-
-	private Action actionResetScene = new AbstractAction("Reset Scene") {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			resetScene();
-			updatePath();
-		}
-	};
-
-	private MouseListener mouseHandler = new MouseAdapter() {
-
-		@Override
-		public void mouseClicked(MouseEvent mouse) {
-			if (mouse.getButton() == MouseEvent.BUTTON1) {
-				int cell = cellAt(mouse.getX(), mouse.getY());
-				setTile(cell, map.get(cell) == WALL ? FREE : WALL);
-				updatePath();
-			}
-		}
-
-		@Override
-		public void mouseReleased(MouseEvent mouse) {
-			if (draggedCell != -1) {
-				// dragging ends
-				draggedCell = -1;
-				updatePath();
-			} else if (mouse.isPopupTrigger()) {
-				popupCell = cellAt(mouse.getX(), mouse.getY());
-				popupMenu.show(canvas, mouse.getX(), mouse.getY());
-			}
-		}
-	};
-
-	private MouseMotionListener mouseMotionHandler = new MouseMotionAdapter() {
-
-		@Override
-		public void mouseDragged(MouseEvent mouse) {
-			int cell = cellAt(mouse.getX(), mouse.getY());
-			if (cell != draggedCell) {
-				// drag enters new cell
-				draggedCell = cell;
-				setTile(cell, mouse.isShiftDown() ? FREE : WALL);
-				updatePath();
-			}
-		}
-	};
 
 	public PathFinderDemoApp(int numCols, int numRows, int canvasSize) {
 		algorithm = PathFinderAlgorithm.AStar;
 		topology = Top8.get();
 		cellSize = canvasSize / numCols;
 		passageWidthPct = 95;
-		popupCell = -1;
-		draggedCell = -1;
-		
+
 		createMap(numCols, numRows, Top8.get());
 		map.fill();
 		source = map.cell(GridPosition.TOP_LEFT);
 		target = map.cell(GridPosition.BOTTOM_RIGHT);
-		
+
 		solution = new BitSet(map.numVertices());
 		createPathFinder();
 		createUI();
@@ -172,21 +81,8 @@ public class PathFinderDemoApp {
 	}
 
 	private void createUI() {
-		canvas = new GridCanvas(map, cellSize);
-		canvas.pushRenderer(createRenderer());
-		canvas.requestFocus();
-		canvas.addMouseListener(mouseHandler);
-		canvas.addMouseMotionListener(mouseMotionHandler);
-		canvas.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-		popupMenu = new JPopupMenu();
-		popupMenu.add(actionSetSource);
-		popupMenu.add(actionSetTarget);
-		popupMenu.addSeparator();
-		popupMenu.add(actionResetScene);
-	
 		window = new PathFinderUI();
 		window.setApp(this);
-		window.add(canvas, BorderLayout.CENTER);
 		window.pack();
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
@@ -232,56 +128,6 @@ public class PathFinderDemoApp {
 		map = newMap;
 	}
 
-	private ConfigurableGridRenderer createRenderer() {
-		// ConfigurableGridRenderer r = new PearlsGridRenderer();
-		ConfigurableGridRenderer r = new WallPassageGridRenderer();
-		r.fnCellSize = () -> cellSize;
-		r.fnCellBgColor = cell -> {
-			if (cell == source) {
-				return Color.GREEN.darker();
-			}
-			if (cell == target) {
-				return Color.BLUE;
-			}
-			if (solution != null && solution.get(cell)) {
-				return Color.RED.brighter();
-			}
-			if (pathFinder != null) {
-				if (pathFinder.getState(cell) == TraversalState.COMPLETED) {
-					return new Color(160, 160, 160);
-				}
-				if (pathFinder.getState(cell) == TraversalState.VISITED) {
-					return new Color(220, 220, 220);
-				}
-			}
-			if (map.get(cell) == WALL) {
-				return new Color(139, 69, 19);
-			}
-			return Color.WHITE;
-		};
-		r.fnText = this::cellText;
-		r.fnTextColor = cell -> {
-			if (cell == source || cell == target) {
-				return Color.WHITE;
-			}
-			if (solution != null && solution.get(cell)) {
-				return Color.WHITE;
-			}
-			return Color.BLUE;
-	
-		};
-		r.fnTextFont = () -> new Font("Arial Narrow", Font.BOLD, cellSize * 40 / 100);
-		r.fnMinFontSize = () -> 4;
-		r.fnPassageWidth = (u, v) -> cellSize * passageWidthPct / 100;
-		r.fnPassageColor = (cell, dir) -> Color.WHITE;
-		return r;
-	}
-
-	public void redraw() {
-		canvas.clear();
-		canvas.drawGrid();
-	}
-
 	public void setAlgorithm(PathFinderAlgorithm alg) {
 		algorithm = alg;
 		createPathFinder();
@@ -311,15 +157,43 @@ public class PathFinderDemoApp {
 		return passageWidthPct;
 	}
 
-	private void setSource(int cell) {
-		source = cell;
+	public BreadthFirstSearch<Tile, Double> getPathFinder() {
+		return pathFinder;
 	}
 
-	private void setTarget(int cell) {
-		target = cell;
+	public BitSet getSolution() {
+		return solution;
 	}
 
-	private void setTile(int cell, Tile tile) {
+	public GridGraph2D<Tile, Double> getMap() {
+		return map;
+	}
+
+	public int getCellSize() {
+		return cellSize;
+	}
+
+	public void setCellSize(int cellSize) {
+		this.cellSize = cellSize;
+	}
+
+	public int getSource() {
+		return source;
+	}
+
+	public void setSource(int source) {
+		this.source = source;
+	}
+
+	public int getTarget() {
+		return target;
+	}
+
+	public void setTarget(int target) {
+		this.target = target;
+	}
+
+	public void setTile(int cell, Tile tile) {
 		if (cell == source || cell == target || map.get(cell) == tile) {
 			return;
 		}
@@ -337,7 +211,7 @@ public class PathFinderDemoApp {
 		});
 	}
 
-	private void resetScene() {
+	public void resetScene() {
 		source = map.cell(GridPosition.TOP_LEFT);
 		target = map.cell(GridPosition.BOTTOM_RIGHT);
 		map.vertices().forEach(cell -> setTile(cell, FREE));
@@ -357,26 +231,12 @@ public class PathFinderDemoApp {
 		System.out.println(String.format("Path cost: %.2f", pathFinder.getCost(target)));
 	}
 
-	private void updatePath() {
+	public void updatePath() {
 		computePath();
-		canvas.drawGrid();
-	}
-	
-	private String cellText(int cell) {
-		if (pathFinder == null || pathFinder.getState(cell) == TraversalState.UNVISITED) {
-			return "";
-		}
-		if (pathFinder instanceof AStarSearch) {
-			AStarSearch<Tile, Double> astar = (AStarSearch<Tile, Double>) pathFinder;
-			return String.format("%.0f", astar.getScore(cell));
-		} else if (pathFinder instanceof BreadthFirstSearch) {
-			BreadthFirstSearch<Tile, Double> bfs = pathFinder;
-			return String.format("%.0f", bfs.getCost(cell));
-		}
-		return "";
+		window.redraw(false);
 	}
 
-	private int cellAt(int x, int y) {
+	public int cellAt(int x, int y) {
 		int gridX = min(x / cellSize, map.numCols() - 1), gridY = min(y / cellSize, map.numRows() - 1);
 		return map.cell(gridX, gridY);
 	}
--- GraphDemos/src/main/java/de/amr/demos/grid/pathfinding/PathFinderUI.java
+++ GraphDemos/src/main/java/de/amr/demos/grid/pathfinding/PathFinderUI.java
@@ -1,33 +1,68 @@
 package de.amr.demos.grid.pathfinding;
 
+import static de.amr.demos.grid.pathfinding.PathFinderDemoApp.Tile.FREE;
+import static de.amr.demos.grid.pathfinding.PathFinderDemoApp.Tile.WALL;
+
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.BorderFactory;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import de.amr.demos.grid.pathfinding.PathFinderDemoApp.PathFinderAlgorithm;
+import de.amr.demos.grid.pathfinding.PathFinderDemoApp.Tile;
 import de.amr.graph.grid.impl.Top4;
 import de.amr.graph.grid.impl.Top8;
+import de.amr.graph.grid.ui.rendering.ConfigurableGridRenderer;
+import de.amr.graph.grid.ui.rendering.GridCanvas;
+import de.amr.graph.grid.ui.rendering.WallPassageGridRenderer;
+import de.amr.graph.pathfinder.api.TraversalState;
+import de.amr.graph.pathfinder.impl.AStarSearch;
+import de.amr.graph.pathfinder.impl.BreadthFirstSearch;
 import net.miginfocom.swing.MigLayout;
 
 public class PathFinderUI extends JFrame {
 
 	private PathFinderDemoApp app;
+
 	private JComboBox<PathFinderAlgorithm> comboAlgorithm;
 	private JComboBox<String> comboTopology;
 	private JSlider sliderPassageWidth;
+	private GridCanvas canvas;
+	private int draggedCell;
+	private int popupCell;
+	private JPopupMenu popupMenu;
 
 	public void setApp(PathFinderDemoApp app) {
 		this.app = app;
+
+		canvas = new GridCanvas(app.getMap(), app.getCellSize());
+		add(canvas, BorderLayout.CENTER);
+		canvas.pushRenderer(createRenderer());
+		canvas.addMouseListener(mouseHandler);
+		canvas.addMouseMotionListener(mouseMotionHandler);
+		canvas.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+		canvas.requestFocus();
+
 		comboAlgorithm.setSelectedItem(app.getAlgorithm());
 		comboTopology.setSelectedItem(app.getTopology() == Top4.get() ? "4 Neighbors" : "8 Neighbors");
 		sliderPassageWidth.setValue(app.getPassageWidthPct());
@@ -88,13 +123,154 @@ public class PathFinderUI extends JFrame {
 					return;
 				}
 				app.setPassageWidthPct(sliderPassageWidth.getValue());
-				app.redraw();
-				System.out.println("New passage width %" + app.getPassageWidthPct());
+				redraw(true);
 			}
 		});
 		sliderPassageWidth.setMinorTickSpacing(10);
 		sliderPassageWidth.setMinimum(1);
 		sliderPassageWidth.setPaintTicks(true);
 		settingsPanel.add(sliderPassageWidth, "cell 1 2");
+
+		popupCell = -1;
+		draggedCell = -1;
+
+		popupMenu = new JPopupMenu();
+		popupMenu.add(actionSetSource);
+		popupMenu.add(actionSetTarget);
+		popupMenu.addSeparator();
+		popupMenu.add(actionResetScene);
 	}
+
+	public void redraw(boolean clear) {
+		if (clear) {
+			canvas.clear();
+		}
+		canvas.drawGrid();
+	}
+
+	private ConfigurableGridRenderer createRenderer() {
+		// ConfigurableGridRenderer r = new PearlsGridRenderer();
+		ConfigurableGridRenderer r = new WallPassageGridRenderer();
+		r.fnCellSize = () -> app.getCellSize();
+		r.fnCellBgColor = cell -> {
+			if (cell == app.getSource()) {
+				return Color.GREEN.darker();
+			}
+			if (cell == app.getTarget()) {
+				return Color.BLUE;
+			}
+			if (app.getSolution() != null && app.getSolution().get(cell)) {
+				return Color.RED.brighter();
+			}
+			if (app.getPathFinder() != null) {
+				if (app.getPathFinder().getState(cell) == TraversalState.COMPLETED) {
+					return new Color(160, 160, 160);
+				}
+				if (app.getPathFinder().getState(cell) == TraversalState.VISITED) {
+					return new Color(220, 220, 220);
+				}
+			}
+			if (app.getMap().get(cell) == WALL) {
+				return new Color(139, 69, 19);
+			}
+			return Color.WHITE;
+		};
+		r.fnText = this::cellText;
+		r.fnTextColor = cell -> {
+			if (cell == app.getSource() || cell == app.getTarget()) {
+				return Color.WHITE;
+			}
+			if (app.getSolution() != null && app.getSolution().get(cell)) {
+				return Color.WHITE;
+			}
+			return Color.BLUE;
+
+		};
+		r.fnTextFont = () -> new Font("Arial Narrow", Font.BOLD, app.getCellSize() * 40 / 100);
+		r.fnMinFontSize = () -> 4;
+		r.fnPassageWidth = (u, v) -> app.getCellSize() * app.getPassageWidthPct() / 100;
+		r.fnPassageColor = (cell, dir) -> Color.WHITE;
+		return r;
+	}
+
+	private MouseListener mouseHandler = new MouseAdapter() {
+
+		@Override
+		public void mouseClicked(MouseEvent mouse) {
+			if (mouse.getButton() == MouseEvent.BUTTON1) {
+				int cell = app.cellAt(mouse.getX(), mouse.getY());
+				app.setTile(cell, app.getMap().get(cell) == WALL ? FREE : WALL);
+				app.updatePath();
+			}
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent mouse) {
+			if (draggedCell != -1) {
+				// dragging ends
+				draggedCell = -1;
+				app.updatePath();
+			} else if (mouse.isPopupTrigger()) {
+				popupCell = app.cellAt(mouse.getX(), mouse.getY());
+				popupMenu.show(canvas, mouse.getX(), mouse.getY());
+			}
+		}
+	};
+
+	private MouseMotionListener mouseMotionHandler = new MouseMotionAdapter() {
+
+		@Override
+		public void mouseDragged(MouseEvent mouse) {
+			int cell = app.cellAt(mouse.getX(), mouse.getY());
+			if (cell != draggedCell) {
+				// drag enters new cell
+				draggedCell = cell;
+				app.setTile(cell, mouse.isShiftDown() ? FREE : WALL);
+				app.updatePath();
+			}
+		}
+	};
+	private Action actionSetSource = new AbstractAction("Set Source Here") {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			app.setSource(popupCell);
+			popupCell = -1;
+			app.updatePath();
+		}
+	};
+
+	private Action actionSetTarget = new AbstractAction("Set Target Here") {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			app.setTarget(popupCell);
+			popupCell = -1;
+			app.updatePath();
+		}
+	};
+
+	private Action actionResetScene = new AbstractAction("Reset Scene") {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			app.resetScene();
+			app.updatePath();
+		}
+	};
+
+	private String cellText(int cell) {
+		if (app.getPathFinder() == null || app.getPathFinder().getState(cell) == TraversalState.UNVISITED) {
+			return "";
+		}
+		if (app.getPathFinder() instanceof AStarSearch) {
+			AStarSearch<Tile, Double> astar = (AStarSearch<Tile, Double>) app.getPathFinder();
+			return String.format("%.0f", astar.getScore(cell));
+		} else if (app.getPathFinder() instanceof BreadthFirstSearch) {
+			BreadthFirstSearch<Tile, Double> bfs = app.getPathFinder();
+			return String.format("%.0f", bfs.getCost(cell));
+		}
+		return "";
+	}
+
 }
