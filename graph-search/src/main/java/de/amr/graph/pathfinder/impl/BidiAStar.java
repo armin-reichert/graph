25320c82c2a09f5fe9f03206a3e6a9d6b5af926f
Armin Reichert <armin.reichert@web.de>
2019-03-25 10:32:13 +0100

    allow for separate heuristics

--- graph-search/src/main/java/de/amr/graph/pathfinder/impl/BidiAStar.java
+++ graph-search/src/main/java/de/amr/graph/pathfinder/impl/BidiAStar.java
@@ -7,8 +7,9 @@ import de.amr.graph.core.api.Graph;
 public class BidiAStar extends BidiGraphSearch<AStarSearch, AStarSearch> {
 
 	public BidiAStar(Graph<?, ?> graph, ToDoubleBiFunction<Integer, Integer> fnEdgeCost,
-			ToDoubleBiFunction<Integer, Integer> fnEstimatedCost) {
-		super(new AStarSearch(graph, fnEdgeCost, fnEstimatedCost),
-				new AStarSearch(graph, fnEdgeCost, fnEstimatedCost));
+			ToDoubleBiFunction<Integer, Integer> fnEstimatedCostForward,
+			ToDoubleBiFunction<Integer, Integer> fnEstimatedCostBackwards) {
+		super(new AStarSearch(graph, fnEdgeCost, fnEstimatedCostForward),
+				new AStarSearch(graph, fnEdgeCost, fnEstimatedCostBackwards));
 	}
 }
