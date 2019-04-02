a1b3ef7df6e948d1936068d525ceeaea46f1ee15
Armin Reichert <armin.reichert@web.de>
2019-04-02 15:50:02 +0200

    improved

--- graph-search/src/main/java/de/amr/graph/pathfinder/impl/BidiAStarSearch.java
+++ graph-search/src/main/java/de/amr/graph/pathfinder/impl/BidiAStarSearch.java
@@ -3,6 +3,7 @@ package de.amr.graph.pathfinder.impl;
 import java.util.function.ToDoubleBiFunction;
 
 import de.amr.graph.core.api.Graph;
+import de.amr.graph.core.api.TraversalState;
 
 public class BidiAStarSearch extends BidiGraphSearch<AStarSearch, AStarSearch> {
 
@@ -11,5 +12,27 @@ public class BidiAStarSearch extends BidiGraphSearch<AStarSearch, AStarSearch> {
 			ToDoubleBiFunction<Integer, Integer> fnEstimatedCostBackwards) {
 		super(new AStarSearch(graph, fnEdgeCost, fnEstimatedCostForward),
 				new AStarSearch(graph, fnEdgeCost, fnEstimatedCostBackwards));
+	}
+
+	@Override
+	public int getParent(int v) {
+		return getForwardSearch().getState(v) != TraversalState.UNVISITED ? getForwardSearch().getParent(v)
+				: getBackwardsSearch().getParent(v);
+	}
+
+	@Override
+	public double getCost(int v) {
+		return getForwardSearch().getState(v) != TraversalState.UNVISITED ? getForwardSearch().getCost(v)
+				: getBackwardsSearch().getCost(v);
+	}
+
+	public double getScore(int v) {
+		return getForwardSearch().getState(v) != TraversalState.UNVISITED ? getForwardSearch().getScore(v)
+				: getBackwardsSearch().getScore(v);
+	}
+
+	public double getEstimatedCost(int v) {
+		return getForwardSearch().getState(v) != TraversalState.UNVISITED ? getForwardSearch().getEstimatedCost(v)
+				: getBackwardsSearch().getEstimatedCost(v);
 	}
 }
