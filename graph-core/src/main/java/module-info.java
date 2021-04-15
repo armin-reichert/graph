6d9adbd3ad380af89db86862121e7807edcb1e93
Armin Reichert <armin.reichert@web.de>
2021-04-15 07:34:45 +0200

    module info

---
+++ graph-core/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module de.amr.graph.core {
+
+	exports de.amr.datastruct;
+	exports de.amr.graph.core.api;
+	exports de.amr.graph.core.impl;
+	exports de.amr.graph.event;
+	exports de.amr.graph.util;
+
+}
