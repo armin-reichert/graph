7961c37b44fe46d932dab85482b0685b8216447b
Armin Reichert <armin.reichert@web.de>
2022-08-14 08:26:06 +0200

    lint, refac

--- graph-grid/src/main/java/de/amr/graph/grid/curves/MooreLCurve.java
+++ graph-grid/src/main/java/de/amr/graph/grid/curves/MooreLCurve.java
@@ -9,8 +9,7 @@ package de.amr.graph.grid.curves;
  * R -> l L f r R f R r f L l </br>
  * </code>
  * <p>
- * with nonterminals <code>{S, L, R}</code>, axiom <code>S</code> and terminals
- * <code>{f, l, r}</code>.
+ * with nonterminals <code>{S, L, R}</code>, axiom <code>S</code> and terminals <code>{f, l, r}</code>.
  * <p>
  * The terminals are interpreted as follows:
  * <ul>
@@ -19,8 +18,8 @@ package de.amr.graph.grid.curves;
  * <li><code>r</code> = turn right (90&deg; clockwise).
  * </ul>
  * <p>
- * On a <code>(n x n)</code>-grid, the curve starts at <code>column = n / 2 - 1, row = n - 1</code>
- * where <code>n</code> has to be a power of 2.
+ * On a <code>(n x n)</code>-grid, the curve starts at <code>column = n / 2 - 1, row = n - 1</code> where <code>n</code>
+ * has to be a power of 2.
  *
  * @author Armin Reichert
  * 
@@ -30,12 +29,26 @@ package de.amr.graph.grid.curves;
 public class MooreLCurve extends TurtleCurve {
 
 	public MooreLCurve(int depth) {
-		S(depth);
+		ss(depth);
 	}
 
 	/*@formatter:off*/
-	void S(int i) {	if (i > 0) { L(i-1); f(); L(i-1); l(); f(); l(); L(i-1); f(); L(i-1); }}
-	void L(int i) { if (i > 0) { r(); R(i-1); f(); l(); L(i-1); f(); L(i-1); l(); f(); R(i-1); r(); }}
-	void R(int i) { if (i > 0) { l(); L(i-1); f(); r(); R(i-1); f(); R(i-1); r(); f(); L(i-1); l(); }}
+	private void ss(int i) {
+		if (i > 0) {
+			ll(i-1); f(); ll(i-1); l(); f(); l(); ll(i-1); f(); ll(i-1); 
+		}
+	}
+	
+	private void ll(int i) {
+		if (i > 0) {
+			r(); rr(i-1); f(); l(); ll(i-1); f(); ll(i-1); l(); f(); rr(i-1); r(); 
+		}
+	}
+	
+	private void rr(int i) {
+		if (i > 0) { 
+			l(); ll(i-1); f(); r(); rr(i-1); f(); rr(i-1); r(); f(); ll(i-1); l(); 
+		}
+	}
 	/*@formatter:on*/
 }
