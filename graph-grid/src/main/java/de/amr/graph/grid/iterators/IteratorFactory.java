2406e2857027adf6c2213c7761e7928593fe4e68
Armin Reichert <armin.reichert@web.de>
2022-08-14 10:00:42 +0200

    lint

--- graph-grid/src/main/java/de/amr/graph/grid/iterators/IteratorFactory.java
+++ graph-grid/src/main/java/de/amr/graph/grid/iterators/IteratorFactory.java
@@ -3,30 +3,27 @@ package de.amr.graph.grid.iterators;
 import java.util.Collection;
 import java.util.Iterator;
 
-public class IteratorFactory<C> {
-
-	private IteratorFactory() {
-	}
+public interface IteratorFactory {
 
 	@SafeVarargs
-	public final static <C> Iterator<C> sequence(Iterator<C>... sources) {
+	public static <C> Iterator<C> sequence(Iterator<C>... sources) {
 		return new SequentialIterator<>(sources);
 	}
 
 	@SuppressWarnings("unchecked")
-	public final static <C> Iterator<C> seq(Collection<Iterator<C>> sources) {
+	public static <C> Iterator<C> seq(Collection<Iterator<C>> sources) {
 		Iterator<?>[] sourcesArray = new Iterator[sources.size()];
 		sourcesArray = sources.toArray(sourcesArray);
 		return sequence((Iterator<C>[]) sourcesArray);
 	}
 
 	@SafeVarargs
-	public final static <C> Iterator<C> parallel(Iterator<C>... sources) {
+	public static <C> Iterator<C> parallel(Iterator<C>... sources) {
 		return new ParallelIterator<>(sources);
 	}
 
 	@SuppressWarnings("unchecked")
-	public final static <C> Iterator<C> par(Collection<Iterator<C>> sources) {
+	public static <C> Iterator<C> par(Collection<Iterator<C>> sources) {
 		Iterator<?>[] sourcesArray = new Iterator[sources.size()];
 		sourcesArray = sources.toArray(sourcesArray);
 		return parallel((Iterator<C>[]) sourcesArray);
